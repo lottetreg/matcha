a029847d34cfddc8ddc9c4e435b82382c4eb1b51
Charlotte Tregelles <charlotte@8thlight.com>
2019-09-26 22:10:49 -0400

    Allow immutable list of routes to be passed into Matcha constructor

--- src/test/java/com/github/lottetreg/matcha/TemplateTest.java
+++ src/test/java/com/github/lottetreg/matcha/TemplateTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.List;
 import java.util.Map;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class TemplateTest {
 
