992f9bf5440af4e7d98ba0a9b7da8b181bb5827c
Charlotte Tregelles <charlotte@8thlight.com>
2019-10-02 15:48:39 -0400

    Escape HTML in Jtwig templates instead of JS

--- src/test/java/com/github/lottetreg/matcha/TemplateTest.java
+++ src/test/java/com/github/lottetreg/matcha/TemplateTest.java
@@ -19,4 +19,15 @@ public class TemplateTest {
 
     assertEquals("\n<h3>how-to-do-something</h3>\n\n", new String(renderedTemplate));
   }
+
+  @Test
+  public void itEscapesHTML() {
+    Template template = new Template("/templates/example.twig.html");
+    List<Post> posts = List.of(new Post(Map.of("slug", "<h1>how-to-do-something</h1>")));
+    Map<String, Object> context = Map.of("posts", posts);
+
+    byte[] renderedTemplate = template.render(context);
+
+    assertEquals("\n<h3>&lt;h1&gt;how-to-do-something&lt;/h1&gt;</h3>\n\n", new String(renderedTemplate));
+  }
 }
